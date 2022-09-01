1bfc7b68a804667065671926fbcb74b3013ccbe9
kenizaya <harveylstark@gmail.com>
2022-09-01 23:29:50 +0530

    Show DropdownMenu onClick

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DropdownMenu from './DropdownMenu'
 import styles from '../styles/Dropdown.module.css'
 
 function Dropdown() {
+  const [isVisible, setIsVisible] = useState(false)
   return (
     <>
-      <button className={styles.dropdown}>3</button>
-      <DropdownMenu />
+      <button
+        className={styles.dropdown}
+        onClick={() => setIsVisible(!isVisible)}
+      >
+        3
+      </button>
+      {isVisible && <DropdownMenu />}
     </>
   )
 }
