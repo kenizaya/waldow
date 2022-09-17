fabfb551243467b8f9562f80605b7f5304e7db9f
kenizaya <harveylstark@gmail.com>
2022-09-17 21:28:59 +0530

    Show target box inside image at an absolute position

--- src/components/MainImage.js
+++ src/components/MainImage.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TargetBox from './TargetBox'
 import image from '../assets/default.jpg'
 
 import styles from '../styles/MainImage.module.css'
+import { useRef } from 'react'
 
 function MainImage() {
+  const [showTargetBox, setShowTargetBox] = useState(false)
+  const mainImageRef = useRef()
   return (
     <>
-      <div>
-        <TargetBox />
+      <div className={styles['target-box']}>
+        {showTargetBox && <TargetBox ref={mainImageRef} />}
       </div>
 
-      <img className={styles.img} src={image} alt='Game' />
+      <img
+        className={styles.img}
+        src={image}
+        alt='Game'
+        onClick={() => setShowTargetBox(!showTargetBox)}
+      />
     </>
   )
 }
--- src/components/TargetBox.js
+++ src/components/TargetBox.js
@@ -4,19 +4,24 @@ import { characters } from '../data/data'
 import bigCursor from '../assets/custom-cursor.svg'
 import styles from '../styles/TargetBox.module.css'
 
-function TargetBox() {
+const TargetBox = React.forwardRef((props, ref) => {
   return (
     <>
-      <img className={styles.cursor} src={bigCursor} alt='big cursor' />
-      <div className={styles.container}>
-        <ul className={styles.list}>
-          {characters.map((character) => (
-            <li className={styles['list-item']}>{character.name}</li>
-          ))}
-        </ul>
+      <div
+        className={styles.root}
+        style={{ backgroundColor: 'red', top: `500px`, left: '600px' }}
+      >
+        <img className={styles.cursor} src={bigCursor} alt='big cursor' />
+        <div className={styles.container}>
+          <ul className={styles.list}>
+            {characters.map((character) => (
+              <li className={styles['list-item']}>{character.name}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   )
-}
+})
 
 export default TargetBox
