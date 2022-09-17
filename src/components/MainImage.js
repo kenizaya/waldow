164d359405feb233713d5662e060d091d6029074
kenizaya <harveylstark@gmail.com>
2022-09-17 21:51:31 +0530

    Show target box inside image at clicked mouse coordinates

--- src/components/MainImage.js
+++ src/components/MainImage.js
@@ -6,19 +6,29 @@ import styles from '../styles/MainImage.module.css'
 import { useRef } from 'react'
 
 function MainImage() {
-  const [showTargetBox, setShowTargetBox] = useState(false)
+  const [showTargetBox, setShowTargetBox] = useState(true)
+  const [coords, setCoords] = useState({ xPos: 0, yPos: 0 })
   const mainImageRef = useRef()
+
+  const handleClick = (event) => {
+    setCoords({
+      xPos: event.pageX,
+      yPos: event.pageY,
+    })
+    setShowTargetBox(!showTargetBox)
+  }
+
   return (
     <>
-      <div className={styles['target-box']}>
-        {showTargetBox && <TargetBox ref={mainImageRef} />}
-      </div>
+      {showTargetBox && (
+        <TargetBox xPos={coords.xPos} yPos={coords.yPos} ref={mainImageRef} />
+      )}
 
       <img
         className={styles.img}
         src={image}
         alt='Game'
-        onClick={() => setShowTargetBox(!showTargetBox)}
+        onClick={handleClick}
       />
     </>
   )
--- src/components/TargetBox.js
+++ src/components/TargetBox.js
@@ -3,13 +3,14 @@ import React from 'react'
 import { characters } from '../data/data'
 import bigCursor from '../assets/custom-cursor.svg'
 import styles from '../styles/TargetBox.module.css'
+import { useState } from 'react'
 
-const TargetBox = React.forwardRef((props, ref) => {
+const TargetBox = React.forwardRef(({ xPos, yPos }, ref) => {
   return (
     <>
       <div
         className={styles.root}
-        style={{ backgroundColor: 'red', top: `500px`, left: '600px' }}
+        style={{ backgroundColor: 'red', top: `${yPos}px`, left: `${xPos}px` }}
       >
         <img className={styles.cursor} src={bigCursor} alt='big cursor' />
         <div className={styles.container}>
