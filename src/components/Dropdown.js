2803903e213d68dec8faab45d8d6545c56e3878c
kenizaya <harveylstark@gmail.com>
2022-09-21 07:35:19 +0530

    Fix header position

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,7 +5,7 @@ import styles from '../styles/Dropdown.module.css'
 function Dropdown() {
   const [isVisible, setIsVisible] = useState(false)
   return (
-    <>
+    <div className={styles.root}>
       <button
         className={styles.dropdown}
         onClick={() => setIsVisible(!isVisible)}
@@ -13,7 +13,7 @@ function Dropdown() {
         3
       </button>
       {isVisible && <DropdownMenu />}
-    </>
+    </div>
   )
 }
 
