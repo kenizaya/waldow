04583f18067d038098522b66d71450476a9667c2
kenizaya <harveylstark@gmail.com>
2022-09-21 21:17:34 +0530

    Link the first game from the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import theLocNar from '../assets/theLocNar.jpg'
+import universe113 from '../assets/universe-113.jpg'
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
   return (
     <>
       <div className={styles.container}>
-        <img src='' alt='' />
-        <img src='' alt='' />
+        <Link to='play'>
+          <img className={styles.img} src={theLocNar} alt='The LOC Nar' />
+        </Link>
+        <img className={styles.img} src={universe113} alt='Universe 113' />
       </div>
     </>
   )
