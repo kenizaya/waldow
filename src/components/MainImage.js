b0b00600a9774288703217a4fe82a005e490caa0
kenizaya <harveylstark@gmail.com>
2022-09-21 10:37:02 +0530

    Hide snackbar after a while

--- src/components/MainImage.js
+++ src/components/MainImage.js
@@ -16,74 +16,65 @@ function MainImage() {
     show: false,
   })
   const [isGameFinished, setIsGameFinished] = useState(false)
-  const [characterLoc, setCharacterLoc] = useState([
-    {
-      name: 'Bowser',
+  const characterLoc = {
+    Bowser: {
       x: 82,
       y: 40.5,
       hasFound: false,
     },
 
-    {
-      name: 'Yubaba',
+    Yubaba: {
       x: 50.39,
       y: 5.42,
       hasFound: false,
     },
-    {
-      name: 'The Knight',
+    'The Knight': {
       x: 57.95,
       y: 91.67,
       hasFound: false,
     },
-  ])
+  }
 
   const imageRef = useRef()
 
   const handleClick = (event) => {
     setMenuLoc({
       x: (event.pageX / imageRef.current.offsetWidth) * 100,
-      y: ((event.pageY - 72) / imageRef.current.offsetHeight) * 100,
+      y: (event.pageY / imageRef.current.offsetHeight) * 100,
     })
     setShowTargetBox(!showTargetBox)
   }
 
   const isClickValid = (x, y, charX, charY) => {
-    console.log(x, charX - 2, charX + 2, y, charY - 2, charY + 2)
     return x > charX - 2 && x < charX + 2 && y > charY - 2 && y < charY + 2
   }
 
   const handleMenuClick = (xPos, yPos, name) => {
-    setCharacterLoc((prevCharacterLoc) => {
-      const newItem = prevCharacterLoc.map((character) => {
-        if (
-          isClickValid(xPos, yPos, character.x, character.y) &&
-          character.name === name
-        ) {
-          setSnackbar({
-            text: `Congrats! You found ${name}!`,
-            bgColor: 'green',
-            show: true,
-          })
-          return { ...character, hasFound: true }
-        } else {
-          setSnackbar({
-            text: `You chose wrong. Keep looking!`,
-            bgColor: 'red',
-            show: true,
-          })
-          return character
-        }
+    if (isClickValid(xPos, yPos, characterLoc[name].x, characterLoc[name].y)) {
+      setSnackbar({
+        text: `Congrats! You found ${name}!`,
+        bgColor: 'green',
+        show: true,
       })
-      return newItem
-    })
-  }
+      characterLoc[name].hasFound = true
+    } else {
+      setSnackbar({
+        text: `You chose wrong. Keep Looking!`,
+        bgColor: 'red',
+        show: true,
+      })
+    }
 
-  useEffect(() => {
-    setIsGameFinished(
-      characterLoc.every((character) => character.hasFound === true)
+    setTimeout(
+      () =>
+        setSnackbar({
+          text: ``,
+          bgColor: '',
+          show: false,
+        }),
+      1800
     )
-  }, [characterLoc])
+  }
 
   return (
     <div className={styles.container} onClick={handleClick} ref={imageRef}>
