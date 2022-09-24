110303e0c57b7ede497e60bddffc9464c82eb95b
kenizaya <harveylstark@gmail.com>
2022-09-24 11:58:43 +0530

    Retrieve images from FireStorage instead of locally

--- src/data/levels.js
+++ src/data/levels.js
@@ -1,26 +1,14 @@
-import robotCity from '../assets/robotCity/robot-city.jpg'
-import goku from '../assets/robotCity/goku.png'
-import mrPoopybutthole from '../assets/robotCity/mrPoopybutthole.png'
-import childEmperor from '../assets/robotCity/childEmperor.png'
-
-import theLocNar from '../assets/theLocNar/theLocNar.jpg'
-import bowser from '../assets/theLocNar/bowser.png'
-import yubaba from '../assets/theLocNar/yubaba.png'
-import knight from '../assets/theLocNar/the-knight.webp'
-
-import universe113 from '../assets/universe113/universe-113.jpg'
-import cacodemon from '../assets/universe113/cacodemon.png'
-import billCypher from '../assets/universe113/bill-cipher.png'
-import courage from '../assets/universe113/courage.png'
-
 export const levels = {
   robotCityData: {
-    board: robotCity,
+    board:
+      'https://firebasestorage.googleapis.com/v0/b/waldow-waldow.appspot.com/o/waldow%2Fassets%2FrobotCity%2Frobot-city.jpg?alt=media&token=1b1b50c1-f72f-4959-91d3-a7da85c9f10c',
+    scores: {},
     data: [
       {
         name: 'Goku',
         from: 'Dragon Ball Z',
-        image: goku,
+        image:
+          'https://firebasestorage.googleapis.com/v0/b/waldow-waldow.appspot.com/o/waldow%2Fassets%2FrobotCity%2Fgoku.png?alt=media&token=83dda4e7-8e2b-4e3f-8770-858f65e8ebb0',
         difficulty: 'easy',
         x: 63.86,
         y: 64.15,
@@ -29,7 +17,8 @@ export const levels = {
       {
         name: 'Mr. Poopy Butthole',
         from: 'Rick and Morty',
-        image: mrPoopybutthole,
+        image:
+          'https://firebasestorage.googleapis.com/v0/b/waldow-waldow.appspot.com/o/waldow%2Fassets%2FrobotCity%2FmrPoopybutthole.png?alt=media&token=7fd67299-687e-4837-8fd9-183da3daa680',
         difficulty: 'medium',
         x: 70.63,
         y: 39.1,
@@ -38,7 +27,8 @@ export const levels = {
       {
         name: 'Child Emperor',
         from: 'One Punch Man',
-        image: childEmperor,
+        image:
+          'https://firebasestorage.googleapis.com/v0/b/waldow-waldow.appspot.com/o/waldow%2Fassets%2FrobotCity%2FchildEmperor.png?alt=media&token=5db3311b-562d-4bdc-931d-717e27f54fe3',
         difficulty: 'hard',
         x: 43.75,
         y: 39.46,
@@ -47,12 +37,15 @@ export const levels = {
     ],
   },
   locData: {
-    board: theLocNar,
+    board:
+      'https://firebasestorage.googleapis.com/v0/b/waldow-waldow.appspot.com/o/waldow%2Fassets%2FtheLocNar%2FtheLocNar.jpg?alt=media&token=b3d7c59e-626b-485a-868b-8af903838043',
+    score: {},
     data: [
       {
         name: 'Bowser',
         from: 'Super Mario Bros',
-        image: bowser,
+        image:
+          'https://firebasestorage.googleapis.com/v0/b/waldow-waldow.appspot.com/o/waldow%2Fassets%2FtheLocNar%2Fbowser.png?alt=media&token=7361e849-015c-4838-90f1-4a4d419d4bd6',
         difficulty: 'easy',
         x: 82,
         y: 40.5,
@@ -61,7 +54,8 @@ export const levels = {
       {
         name: 'Yubaba',
         from: 'Spirited Away',
-        image: yubaba,
+        image:
+          'https://firebasestorage.googleapis.com/v0/b/waldow-waldow.appspot.com/o/waldow%2Fassets%2FtheLocNar%2Fyubaba.png?alt=media&token=d7e6f91f-60b8-4081-8226-8bb27838da45',
         difficulty: 'medium',
         x: 50.39,
         y: 5.42,
@@ -70,7 +64,8 @@ export const levels = {
       {
         name: 'The Knight',
         from: 'Hollow Knight',
-        image: knight,
+        image:
+          'https://firebasestorage.googleapis.com/v0/b/waldow-waldow.appspot.com/o/waldow%2Fassets%2FtheLocNar%2Fthe-knight.webp?alt=media&token=4eff1e9b-d37a-4e66-bafe-da35441c3609',
         difficulty: 'hard',
         x: 57.95,
         y: 91.67,
@@ -80,12 +75,15 @@ export const levels = {
   },
 
   uniData: {
-    board: universe113,
+    board:
+      'https://firebasestorage.googleapis.com/v0/b/waldow-waldow.appspot.com/o/waldow%2Fassets%2Funiverse113%2Funiverse-113.jpg?alt=media&token=b9084dfa-c88c-4412-a40f-0d7b685d9eca',
+    scores: {},
     data: [
       {
         name: 'Cacodemon',
         from: 'DOOM',
-        image: cacodemon,
+        image:
+          'https://firebasestorage.googleapis.com/v0/b/waldow-waldow.appspot.com/o/waldow%2Fassets%2Funiverse113%2Fcacodemon.png?alt=media&token=2539cb26-3b58-4604-aae1-6a7321b82b14',
         difficulty: 'easy',
         x: 50.26,
         y: 26.03,
@@ -94,7 +92,8 @@ export const levels = {
       {
         name: 'Bill Cypher',
         from: 'Gravity Falls',
-        image: billCypher,
+        image:
+          'https://firebasestorage.googleapis.com/v0/b/waldow-waldow.appspot.com/o/waldow%2Fassets%2Funiverse113%2Fbill-cipher.png?alt=media&token=6d5dd6d9-5192-4fc2-b5b4-99b39d26d430',
         difficulty: 'medium',
         x: 69.42,
         y: 65.17,
@@ -103,7 +102,8 @@ export const levels = {
       {
         name: 'Courage',
         from: 'Courage the Cowardly Dog',
-        image: courage,
+        image:
+          'https://firebasestorage.googleapis.com/v0/b/waldow-waldow.appspot.com/o/waldow%2Fassets%2Funiverse113%2Fcourage.png?alt=media&token=2daeaee9-1164-4702-ab06-73e660d35614',
         difficulty: 'hard',
         x: 29.76,
         y: 52.23,
